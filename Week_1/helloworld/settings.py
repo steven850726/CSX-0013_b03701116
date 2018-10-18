df2f42c2de065efd3bc7e44e91c0bdc493ff9e5b
Steven Lin <stevenlin@StevendeMacBook-Air.local>
2018-10-18 21:10:44 +0800

    Week5HW
    Form not done yet

--- Week_1/helloworld/settings.py
+++ Week_1/helloworld/settings.py
@@ -75,10 +75,18 @@ WSGI_APPLICATION = 'helloworld.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-     }
+#     'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#  }
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testdb',
+        'USER': 'helloworld',
+        'PASSWORD': ' ',
+        'HOST': '',
+        'PORT': '',
+    }
  }
 #import dj_database_url
 #DATABASES = {
--- Week_1/helloworld/views.py
+++ Week_1/helloworld/views.py
@@ -7,9 +7,22 @@ from guestbook.models import Vocabulary
 
 
 def index(request):
-	v1 = Vocabulary.objects.create(vocab='한국', meaning='韓國')
-	v2 = Vocabulary.objects.create(vocab='공책', meaning='筆記本')
+	#v1 = Vocabulary.objects.create(vocab='한국', meaning='韓國')
+	#v2 = Vocabulary.objects.create(vocab='공책', meaning='筆記本')
 
 	questions = Vocabulary.objects.all()
 	#questions = ['韓國','筆記本','三']
 	return render(request, 'guestbookver1.html', locals())
+
+
+def create(request):
+
+		#if request.method == 'POST':
+		#	form = Vocabulary(request.POST)
+		#	if form.is_valid():
+		#		form.save()
+		#		return redirect(create.get_absolute_url())
+				return render(request,'create.html',locals())
+		#else:
+		#	form = Vocabulary()
+		#	return render(request, 'create.html', {'form': form})
